dde225ee9d1677a635546b2ddbbecb11d8f98f55
carrotteddy <devcarrott@gmail.com>
2023-06-22 20:18:55 +0600

    Initial commit

---
+++ src/Magic.java
@@ -0,0 +1,7 @@
+public class Magic extends Hero {
+
+    @Override
+    public void applySuperAbility() {
+        System.out.println("Magical применил суперспособность вызова огненной стены");
+    }
+}
